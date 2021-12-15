7f2d6260cc802010c50f910508b717fadbf6f0e5
omaralezzi <81753019+omaralezzi@users.noreply.github.com>
2021-12-15 21:49:09 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import Button from './components/Button';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <main>
+      <Header />
+      <h1>Welcome to React</h1>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <Footer />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+const Button = () => {
+  return <button>submit</button>;
+};
+
+export default Button;
